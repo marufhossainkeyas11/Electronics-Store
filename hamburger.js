e26413f5358c4d3cb192d13a4a50d2c11242d1b8
marufhossainkeyas11 <166496971+marufhossainkeyas11@users.noreply.github.com>
2025-08-15 12:53:44 +0600

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -13,3 +13,29 @@ hamburger.addEventListener('keydown', (e) => {
     navMenu.classList.toggle('active');
   }
 });
+const sBar = document.querySelector('.s_bar input');
+const sBtn = document.querySelector('.s_bar button');
+
+function runSearch() {
+  if (!sBar.value) return;
+  
+  sessionStorage.setItem('searchValue', sBar.value);
+  sBar.value = '';
+  window.location.href = '/Electronics-Store/search.html';
+}
+
+sBtn.addEventListener('click', runSearch);
+
+sBar.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    runSearch();
+  }
+});
+
+if (window.location.pathname === '/Electronics-Store/search.html') {
+  const savedValue = sessionStorage.getItem('searchValue');
+  if (savedValue) {
+    sBar.value = savedValue;
+    sessionStorage.setItem('searchValue', '');
+  }
+}
